a494fe1f8688479796291b9ad333633fe7b118ac
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-09-19 17:08:44 -0500

    Initial commit

---
+++ app/src/main/java/com/shantanoo/flixster/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shantanoo.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
